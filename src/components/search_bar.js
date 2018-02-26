62e451cdba99455855e81dca11171025cc059c8e
Hadi Yousufi <reach.hadiy@gmail.com>
2018-02-26 13:41:42 -0600

    adds initial state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 // define a new class called Searchbar and give it all the functionality of React.Component
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+}
+
   render() {
     // returns an input box with an event handler attached that watches and runs the oninputchange func
     return <input onChange={event => console.log(event.target.value)} />;
