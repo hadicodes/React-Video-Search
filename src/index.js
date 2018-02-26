ebab1905d96b6c138a9508ab50aa26649514a360
Hadi Yousufi <reach.hadiy@gmail.com>
2018-02-25 23:43:47 -0600

    SerachBar component is rendering successfully

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
 const API_KEY = "AIzaSyBxQ5Qgf13mwPIrQsEVWmPmzPVqyfoxenk";
 
 // Create a new component that should produce some HTML
 const App = () => {
-  return <div>Yay the JSX is being rendered to the DOM!</div>;
+  return (
+    <div>
+      <SearchBar />  
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put in onto the DOM
