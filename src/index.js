7faeb94662f7913aeee241e609af0268ab8708c0
Hadi Yousufi <reach.hadiy@gmail.com>
2018-02-25 20:21:25 -0600

    adds youtube APi v3 key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const API_KEY = "AIzaSyBxQ5Qgf13mwPIrQsEVWmPmzPVqyfoxenk";
 
 // Create a new component that should produce some HTML
 const App = () => {
   return <div>Yay the JSX is being rendered to the DOM!</div>;
 };
 
-// Take this components generated HTML and put in onto the DOM
+// Take this component's generated HTML and put in onto the DOM
 ReactDOM.render(<App />, document.querySelector(".container"));
